4b5cdb311c844791da1c1bad3c3602458c830dee
Nolan Holden <nolanholden@users.noreply.github.com>
2018-01-11 10:15:37 -0500

    show register usage (32 total)

--- asm-test/main.asm
+++ asm-test/main.asm
@@ -1,4 +1,23 @@
 
 start:
-    inc r16
+	
+	inc r0
+	inc r1
+	inc r2
+	inc r3
+	inc r4
+	inc r5
+	inc r6
+	inc r7
+	inc r8
+	inc r9
+	inc r10
+	inc r11
+	inc r12
+	inc r13
+	inc r14
+	inc r15
+	inc r16
+	inc r17
+	inc r31
     rjmp start
